fa30631704fec86ce59597468b2b2e0881f2d4da
rkrish00568 <143256126+rkrish00568@users.noreply.github.com>
2025-03-16 14:51:07 +0530

    Update defines.v

--- defines.v
+++ defines.v
@@ -1,11 +1,1 @@
-        
-/*   
-
-Author : VLSI Excellence - Gyan Chand Dhaka (https://www.youtube.com/@vlsiexcellence)
-
-*/
-
-// Code your design here
-
-//`define UART_TX_ONLY 
-//`define UART_RX_ONLY 
+// made by Krishnendu Roy
